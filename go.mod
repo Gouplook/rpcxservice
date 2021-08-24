195dc68f1555d48ca09a88fa66c8024ad534805d
yinjinlin <yinjinlin_uplook@163.com>
2021-08-24 17:38:17 +0800

    ..

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module service_B
 go 1.16
 
 require (
-	github.com/rpcxio/rpcx-examples v1.1.6 // indirect
-	github.com/smallnest/rpcx v1.6.9 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0
+	github.com/rpcxio/rpcx-examples v1.1.6
+	github.com/smallnest/rpcx v1.6.9
+	google.golang.org/grpc/examples v0.0.0-20210818220435-8ab16ef276a3 // indirect
 )
--- main.go
+++ main.go
@@ -11,13 +11,19 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"github.com/rcrowley/go-metrics"
 	example "github.com/rpcxio/rpcx-examples"
 	"github.com/smallnest/rpcx/server"
+	"github.com/smallnest/rpcx/serverplugin"
+	"log"
+	"time"
 )
 
 
 var (
-	addr = flag.String("addr","localhost:8972","servic address")
+	addr = flag.String("addr","localhost:10002","service address")
+	zkAddr = flag.String("zkAddr","localhost:2181","zookeeper address")
+	basePath = flag.String("base","Users/uplook/youpin","prefix path")
 )
 
 type Arith struct {
@@ -43,8 +49,14 @@ func (a *Arith) Add (ctx context.Context,args example.Args, reply *example.Reply
 func main(){
 
 	flag.Parse()
+	// 创建一个服务实例。
 	s := server.NewServer()
+
+	// 注册中心zookeeper 插件必须在注册服务之前添加到Server中
+	addRegistryPlugin(s)
 	err := s.RegisterName("Arith",new(Arith), "")
+
+
 	if err != nil {
 		panic(err)
 	}
@@ -53,5 +65,28 @@ func main(){
 		panic(err)
 	}
 
+	func (){
+		defer s.Close()
+	}()
+
 	fmt.Println("service ....")
+}
+
+// 添加注册中心插件 ZooKeeper
+func addRegistryPlugin(s *server.Server) {
+	r := &serverplugin.ZooKeeperRegisterPlugin{
+		ServiceAddress: "tcp@" + *addr,
+		ZooKeeperServers: []string{*zkAddr},
+		BasePath: *basePath,
+		Metrics: metrics.NewRegistry(), // 用来更新服务TPS
+		UpdateInterval: time.Minute,  // 更新时间
+	}
+
+	err := r.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s.Plugins.Add(r)
+
 }
